4bc340849e91dcde8ab2bde4956c97b2165acd28
Remy Glaser <rglaser@gld.nl>
2026-09-15 17:34:27 +0200

    fix webpack prod config

--- webpack.production.js
+++ webpack.production.js
@@ -1,5 +1,4 @@
 const { merge } = require("webpack-merge");
-const TerserPlugin = require("terser-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 const common = require("./webpack.common.js");
@@ -59,12 +58,6 @@ module.exports = merge(common, {
     }),
   ],
   optimization: {
-    minimize: true,
-    minimizer: [
-      new TerserPlugin({
-        extractComments: false,
-      }),
-      new CssMinimizerPlugin(),
-    ],
+    minimizer: ["...", new CssMinimizerPlugin()],
   },
 });
